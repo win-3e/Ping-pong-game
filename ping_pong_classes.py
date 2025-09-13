bd7ac39b53304f8ebcd76ed74ded7f3b096f0d38
win-3e <buniwin2812@gmail.com>
2025-09-13 12:55:26 +0700

    Update ping_pong_classes.py

--- ping_pong_classes.py
+++ ping_pong_classes.py
@@ -8,8 +8,23 @@ class GameSprite(sprite, Sprite):
     self.image.get_rect()
     self.rect.x = player_x
     self.rect.y = player_y
+  def reset(self):
+    window.blit(self.image (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-  def update(self):
+  def update_l(self):
     keys = Key.get_pressed()
-    #if keys = [K_LEFT] and self.rect.x
+    if keys = [K_w] and self.rect.y > 5:
+      self.rect.y -= self.speed
+    if keys = [K_s] and self.rect.y < (500-65):
+      self.rect.y += self.speed
+  def update_r(self):
+    keys = Key.get_pressed()
+    if keys = [K_UP] and self.rect.y >5:
+      self.rect.y -= self.speed
+    if keys = [K_DOWN] and self.rect.y < (500-65):
+      self.rect.y += self.speed
+    
+    
+
+
