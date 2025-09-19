1bf19394cd65e239c2c03cb00d8307a5e43b5798
winnie <buniwin2812@gmail.com>
2025-09-19 22:01:49 +0700

    finalized ping pong game

--- ping_pong_classes.py
+++ ping_pong_classes.py
@@ -1,28 +1,30 @@
 from pygame import *
 
-class GameSprite(sprite, Sprite):
-  def __init__(self, player_image, player_x, player_y, player_speed, player_width, player_height):
+class GameSprite(sprite.Sprite):
+  def __init__(self, p_image, p_x, p_y, p_width, p_height, p_speed):
     super().__init__()
-    self.image = transform.scale(image.load(player_image))
-    self.speed = player_speed
-    self.image.get_rect()
-    self.rect.x = player_x
-    self.rect.y = player_y
-  def reset(self):
-    window.blit(self.image (self.rect.x, self.rect.y))
+
+    self.image = transform.scale(image.load(p_image), (p_width, p_height))
+    self.speed = p_speed
+    self.rect = self.image.get_rect()
+    self.rect.x = p_x
+    self.rect.y = p_y
+  def reset(self, window):
+    window.blit(self.image ,(self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-  def update_l(self):
-    keys = Key.get_pressed()
-    if keys = [K_w] and self.rect.y > 5:
+  def update_1(self):
+    keys = key.get_pressed()
+    if keys[K_w] and self.rect.y > 5:
       self.rect.y -= self.speed
-    if keys = [K_s] and self.rect.y < (500-65):
+    if keys[K_s] and self.rect.y < (500-65):
       self.rect.y += self.speed
-  def update_r(self):
-    keys = Key.get_pressed()
-    if keys = [K_UP] and self.rect.y >5:
+
+  def update_2(self):
+    keys = key.get_pressed()
+    if keys[K_UP] and self.rect.y >5:
       self.rect.y -= self.speed
-    if keys = [K_DOWN] and self.rect.y < (500-65):
+    if keys[K_DOWN] and self.rect.y < (500-65):
       self.rect.y += self.speed
     
     
--- ping_pong_main.py
+++ ping_pong_main.py
@@ -1,18 +1,59 @@
 # Ping-pong-game 
 
 from pygame import *
+from ping_pong_classes import GameSprite, Player
 
-background= (200,255,255) 
 window = display.set_mode((600,500))
-window.fill(background)
+background = transform.scale(image.load('ping_table.png'),(600,500))
 
 clock = time.Clock()
-FPS = 60run = True
+ball = GameSprite('tennis_ball.png', p_x = 50 ,p_y =50, p_width = 60, p_height = 60, p_speed= 2)
+racket1 = Player('racket.png', p_x=5, p_y=240, p_width=30, p_height=150, p_speed = 4)
+racket2 = Player('racket.png', p_x=560, p_y=240, p_width=30, p_height=150, p_speed=4)
+
+font.init()
+font1 = font.SysFont('Arial', 35)
+player1_lose = font1.render('PLAYER 1 LOST', True, (255,255,0))
+font2 = font.SysFont('Arial', 35)
+player2_lose = font2.render('PLAYER 2 LOST', True, (255,255,0))
+
+speed_x = 2
+speed_y = 2
+
+run = True
+finish = True
 
 while run: 
   for e in event.get():
     if e.type == QUIT:
       run = False
+    
+  if finish:
+    window.blit(background, (0,0))
+    ball.reset(window)
+
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
+
+    if ball.rect.y >= 440 or ball.rect.y <=0 :
+      speed_y *= -1
+    
+    if sprite.collide_rect(racket2, ball) or sprite.collide_rect(racket1, ball):
+      speed_x *= -1
+
+    if ball.rect.x <= 0:
+      window.blit(player1_lose, (200,200))
+      finish = False
+    
+    if ball.rect.x >= 600:
+      window.blit(player2_lose, (200,200))
+      finish = False
+
+    racket1.reset(window)
+    racket2.reset(window)
+
+    racket1.update_1()
+    racket2.update_2()
 
   display.update()
-  clock.tick(FPS)
+  clock.tick(60)
